13fb4b16d94304b51e1113be43554cd8ddc18b74
Elías Puma <elpumitaelias@gmail.com>
2017-07-21 09:27:40 -0500

    Added clustering files

--- backend/clustering/src/main.rs
+++ backend/clustering/src/main.rs
@@ -3,18 +3,29 @@ extern crate kernel;
 extern crate clustering;
 extern crate rustc_serialize;
 extern crate csv;
+extern crate cogset;
 
 use std::collections::Bound::Included;
+use cogset::{Dbscan, BruteScan, Euclid};
 
 fn main() {
     let curr_time = kernel::current_time(); // Current Time
-    let bound_time = kernel::weeks_ago(15); // Current Time - x Weeks
+    let bound_time = kernel::weeks_ago(5); // Current Time - x Weeks
 
     let records = record::read_records_from("../../data/data1617.csv"); // Load records
     let tree = clustering::make_record_tree(&records);                  // Store records in a BTree
 
     let range_records = tree.range((Included(&bound_time), Included(&curr_time))); // Filter records in range
+    /**********************************************************/
     for record in range_records {
         println!("{:?}", record);
     }
+    /**********************************************************/
+    // // let scanner = BruteScan::new(&range_records.map(|x| *x.1.clone()).collect());
+    // let scanner = BruteScan::new(&records);
+    // let mut dbscan = Dbscan::new(scanner, 0.2, 2);
+
+    // let clusters = dbscan.by_ref().collect::<Vec<_>>();
+
+    // println!("{:#?}", clusters);
 }
--- libs/record/src/lib.rs
+++ libs/record/src/lib.rs
@@ -1,10 +1,13 @@
 extern crate quick_csv;
 extern crate rustc_serialize;
 extern crate chrono;
+extern crate cogset;
 
 use quick_csv::Csv;
 // use std::io::{self, Write, Read};
 use chrono::{NaiveDate, NaiveDateTime};
+use cogset::Point;
+use std::str::FromStr;
 
 #[derive(Debug, Clone, RustcDecodable, RustcEncodable)]
 pub struct Record {
@@ -15,11 +18,11 @@ impl Record {
     pub fn get_date_time(&self) -> NaiveDateTime {
         // println!("D: {}, T: {}, WD: {}",
         //         //  self.values[3],
-                // //  self.values[4],
-                //  self.values[16]);
+        // //  self.values[4],
+        //  self.values[16]);
         let date: Vec<&str> = self.values[3].split("/").collect();
         let mut time: Vec<&str> = self.values[4].split(":").collect();
-        
+
         if time.len() == 1 {
             time.pop();
             time.push("00");
@@ -33,12 +36,26 @@ impl Record {
                 .and_hms(u32::from_str_radix(time[0], 10).unwrap(),
                          u32::from_str_radix(time[1], 10).unwrap(),
                          u32::from_str_radix(time[2], 10).unwrap());
-        
+
         dt
     }
 
     pub fn get_lat_lon(&self) -> (String, String) {
         (self.values[22].clone(), self.values[21].clone())
+    }
+}
+
+impl Point for Record {
+    fn dist(&self, other: &Record) -> f64 {
+        let lat1 = f64::from_str(self.values[22].as_ref()).unwrap();
+        let lon1 = f64::from_str(self.values[21].as_ref()).unwrap();
+        let lat2 = f64::from_str(other.values[22].as_ref()).unwrap();
+        let lon2 = f64::from_str(other.values[21].as_ref()).unwrap();
+
+        let mut distance = (lat1 - lat2).powf(2.0);
+        distance += (lon1 - lon2).powf(2.0);
+
+        distance.sqrt()
     }
 }
 
