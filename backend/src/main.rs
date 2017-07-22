ea21dc9ee510a33f296e70b780f05e53b89be06d
Elías Puma <elpumitaelias@gmail.com>
2017-07-22 10:16:47 -0500

    Added styles and getting right output

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,5 @@
 extern crate record;
 extern crate kernel;
-
 /************************************************************************/
 extern crate rustc_serialize;
 extern crate csv;
@@ -8,35 +7,61 @@ extern crate csv;
 #[derive(RustcEncodable)]
 pub struct NaiveRecord(String, String, f64);
 /************************************************************************/
+use std::io::prelude::*;
+use std::fs::File;
+/************************************************************************/
 fn main() {
     let curr_time = kernel::current_time();
     let records = record::read_records_from("../data/data1617.csv");
-    let filtrd: Vec<(String, String, f64)> = Vec::new();
-    // let ker_sum = kernel::kernel_sum(&rec[10000], &curr_time);
-    // println!("{}", ker_sum);
-    let mut wtr = csv::Writer::from_file("../frontend/kde.csv").unwrap();
-    for (i, rec) in records.iter().enumerate() {
+    // let filtrd: Vec<(String, String, f64)> = Vec::new();
+
+    // let mut wtr = csv::Writer::from_file("../frontend/kde.csv").unwrap();
+    // for (i, rec) in records.iter().enumerate() {
+    //     let ker_sum = kernel::kernel_sum(&rec, &curr_time);
+    //     if ker_sum > 0.6 {
+    //         println!("{}> {}\n", i, ker_sum);
+    //         let (lat, lon) = rec.get_lat_lon();
+    //         let record = NaiveRecord(lat, lon, ker_sum * 10.0); //"lat","lon","weight"
+    //         wtr.encode(record);
+    //     }
+    // }
+
+    let mut output = File::create("../frontend/heatmap.js").expect("Error");
+    output
+        .write_all(b"function getPoints() {
+    return [
+        ")
+        .expect("Error writing first part");
+
+    let mut first = true;
+    for rec in &records {
         let ker_sum = kernel::kernel_sum(&rec, &curr_time);
-        if ker_sum > 0.6 {
-            println!("{}> {}\n", i, ker_sum);
+        if ker_sum > 0.4 {
+            // THRESHOLD
+            if !first {
+                write!(output, ",\n\t\t").expect("Error in line break");
+            } else {
+                first = false;
+            }
+
             let (lat, lon) = rec.get_lat_lon();
-            let record = NaiveRecord(lat, lon, ker_sum * 10.0);    //"lat","lon","weight"
-            wtr.encode(record);
+            write!(output,
+                   "{{ location: new google.maps.LatLng({}, {}), weight: {} }}",
+                   lat,
+                   lon,
+                   ker_sum).expect("Error writing record");
         }
     }
 
-    println!("Hello world!");
+    output
+        .write_all(b"    ];
 }
 
-// extern crate iron;
-// extern crate geojson;
+heatmap = new google.maps.visualization.HeatmapLayer({
+    data: getPoints(),
+    map: map
+});")
+        .expect("Error writing third part");
 
-// use geojson::GeoJson;
-// use iron::prelude::*;
-// use iron::status;
-
-// fn main() {
-//     Iron::new(|_: &mut Request| {
-//         Ok(Response::with((status::Ok, "Hello World!")))
-//     }).http("localhost:3000").unwrap();
-// }
+    println!("Hello world!");
+}
--- frontend/script.js
+++ frontend/script.js
@@ -6,30 +6,20 @@ var map, heatmap;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: { lat: 37.782551, lng: -122.445368 },
+        zoom: 12,
+        center: { lat: 33.754504, lng: -84.396582 },
         mapTypeId: 'roadmap'
     });
 
-    map.data.loadGeoJson("./sample.json");
-
-    // Set the stroke width, and fill color for each polygon
     map.data.setStyle({
         fillColor: 'green',
         strokeWeight: 1
     });
 
-    heatmap = new google.maps.visualization.HeatmapLayer({
-        data: getPoints(),
-        map: map
-    });
-}
+    var script = document.createElement('script');
+    script.src = './heatmap.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
 
-function toggleHeatmap() {
-    heatmap.setMap(heatmap.getMap() ? null : map);
-}
-
-function changeGradient() {
     var gradient = [
         'rgba(0, 255, 255, 0)',
         'rgba(0, 255, 255, 1)',
@@ -46,8 +36,32 @@ function changeGradient() {
         'rgba(191, 0, 31, 1)',
         'rgba(255, 0, 0, 1)'
     ]
-    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+    heatmap.set('gradient', gradient);
 }
+
+function toggleHeatmap() {
+    heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+// function changeGradient() {
+//     var gradient = [
+//         'rgba(0, 255, 255, 0)',
+//         'rgba(0, 255, 255, 1)',
+//         'rgba(0, 191, 255, 1)',
+//         'rgba(0, 127, 255, 1)',
+//         'rgba(0, 63, 255, 1)',
+//         'rgba(0, 0, 255, 1)',
+//         'rgba(0, 0, 223, 1)',
+//         'rgba(0, 0, 191, 1)',
+//         'rgba(0, 0, 159, 1)',
+//         'rgba(0, 0, 127, 1)',
+//         'rgba(63, 0, 91, 1)',
+//         'rgba(127, 0, 63, 1)',
+//         'rgba(191, 0, 31, 1)',
+//         'rgba(255, 0, 0, 1)'
+//     ]
+//     heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+// }
 
 function changeRadius() {
     heatmap.set('radius', heatmap.get('radius') ? null : 20);
@@ -57,12 +71,12 @@ function changeOpacity() {
     heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
 }
 
-function getPoints() {
-    // var records = 
-    // console.log(records);
-    return [
-        new google.maps.LatLng(37.782551, -122.445368),
-        new google.maps.LatLng(37.782745, -122.444586),
-        new google.maps.LatLng(37.782842, -122.443688),
-        new google.maps.LatLng(37.782919, -122.442815)];
-}
+// function getPoints() {
+//     // var records = 
+//     // console.log(records);
+//     return [
+//         new google.maps.LatLng(37.782551, -122.445368),
+//         new google.maps.LatLng(37.782745, -122.444586),
+//         new google.maps.LatLng(37.782842, -122.443688),
+//         new google.maps.LatLng(37.782919, -122.442815)];
+// }
