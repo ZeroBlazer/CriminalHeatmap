00c9427f8748cf5bf3b253360c7740800963f2d4
Elías Puma <elpumitaelias@gmail.com>
2017-07-23 19:42:10 -0500

    Minor changes

--- backend/clustering/src/main.rs
+++ backend/clustering/src/main.rs
@@ -12,7 +12,7 @@ use geojson::{Feature, GeoJson, Geometry, Value};
 
 fn main() {
     let curr_time = kernel::current_time(); // Current Time
-    let bound_time = kernel::weeks_ago(17); // Current Time - x Weeks
+    let bound_time = kernel::weeks_ago(9); // Current Time - x Weeks
 
     let records = record::read_records_from("../../data/data1617.csv"); // Load records
     let tree = clustering::make_record_tree(&records); // Store records in a BTree
@@ -32,7 +32,40 @@ fn main() {
         }
     }
     println!("{:#?}", clusters);
+    /**********************************************************/
+    let mut features = Vec::new();
 
+    for record in &geo_records {
+        let mut properties = Map::new();
+
+        properties.insert("description".to_string(),
+                          value::Value::String(record.get_description()));
+        properties.insert("crime_type".to_string(),
+                          value::Value::String(record.get_crime_type()));
+
+        let (lat, lon) = record.get_lat_lon();
+
+        let geojson = Feature {
+            bbox: None,
+            geometry: Some(Geometry::new(Value::Point(vec![lon, lat]))),
+            foreign_members: None,
+            id: None,
+            properties: Some(properties),
+        };
+
+        features.push(geojson);
+    }
+
+    let feature_collection = GeoJson::FeatureCollection(FeatureCollection {
+                                                            bbox: None,
+                                                            foreign_members: None,
+                                                            features: features,
+                                                        });
+
+    let mut geo_out = File::create("../frontend/cluster.json").expect("Error creating file");
+    write!(geo_out, "{}", feature_collection).expect("Error writing geo info");
+    /************************************************************************/
+    println!("Hello world!");
     // /*******************************CLUSTERING*******************************/
     // // let range_records = tree.range((Included(&bound_time), Included(&curr_time)));
     // // let query_records: Vec<record::Record> = range_records.map(|(_, &r)| r.clone()).collect(); // Vec containing cloned query records
